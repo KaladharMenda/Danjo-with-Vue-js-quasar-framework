2b2e6817208e325e0d737ce0cc0186ce87b65a3d
mkaladhar <kaladhar@headrun.com>
2019-03-06 18:27:35 +0530

    dependencies

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,17 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QCarousel,QCarouselSlide,QCarouselControl,QSelect,QField,QInput,QCard,QCardTitle,QCardMain,QUploader,QSpinner,QTabs,QTab,QTabPane,QStepper,QStep,QStepperNavigation,QInnerLoading,QDatetimePicker,QDatetime,QTable,QTh,QTr,QTd,QTableColumns,QModal,QSearch,QToggle,QCollapsible,QPopover,QModalLayout,QAutocomplete,QCardSeparator,QCardActions,QCardMedia,QChip,QRouteTab,Ripple,CloseOverlay,Notify} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QCarousel,QCarouselSlide,QCarouselControl,QSelect,QField,QInput,QCard,QCardTitle,QCardMain,QUploader,QSpinner,QTabs,QTab,QTabPane,QStepper,QStep,QStepperNavigation,QInnerLoading,QDatetimePicker,QDatetime,QTable,QTh,QTr,QTd,QTableColumns,QModal,QSearch,QToggle,QCollapsible,QPopover,QModalLayout,QAutocomplete,QCardSeparator,QCardActions,QCardMedia,QChip,QRouteTab},directives: {Ripple,CloseOverlay},plugins: {Notify} })
