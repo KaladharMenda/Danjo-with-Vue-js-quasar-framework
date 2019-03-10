29c37d995e4d7127d8f124ed8c2ce2dc46000ab6
Avadhanisharma <avadhani@headrun.com>
2019-03-10 22:53:34 +0530

    adding update function

--- backend/govweb/urls.py
+++ backend/govweb/urls.py
@@ -6,5 +6,7 @@ from views import *
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^student_details/$', student_details),
+    url(r'^get_student_details/$', get_student_details),
+
 
 ]
