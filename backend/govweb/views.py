f01427f794af439cda8493b89f2c853c6937cc2e
Avadhanisharma <avadhani@headrun.com>
2019-03-10 22:51:25 +0530

    adding update function

--- backend/govweb/views.py
+++ backend/govweb/views.py
@@ -18,8 +18,41 @@ def student_details(request):
         Student_details.objects.create(**data_dict)
     except Exception as e:
         import traceback
-
-
-
-
+        return HttpResponse(e)
     return HttpResponse("Success")
+@csrf_exempt
+def get_student_details(request):
+    pin = json.loads(request.POST.keys()[0])
+    student_details = Student_details.objects.filter(pin = pin)
+    if student_details.exists():
+        student_details = student_details[0]
+        data_dict ={}
+        data_dict['student_name'] = student_details.student_name
+        data_dict['pin'] = student_details.pin
+        data_dict['gender'] = student_details.gender
+        data_dict['date_of_birth'] = student_details.date_of_birth.strftime("%d %m %Y")
+        data_dict['relative_name'] =student_details.relative_name
+        data_dict['relation_ship'] =student_details.relation_ship
+        data_dict['caste'] = student_details.caste
+        data_dict['qualification']  = student_details.qualification
+        data_dict['joining_date'] = student_details.joining_date.strftime("%d %m %Y")
+        data_dict['ph'] = student_details.ph
+        data_dict['mole_one'] = student_details.mole_one
+        data_dict['mole_two'] = student_details.mole_two
+        data_dict['tenth_hallticket'] = student_details.tenth_hallticket
+        data_dict['tenth_passed_year'] = student_details.tenth_passed_year
+        data_dict['scheme_code'] = student_details.scheme_code
+        data_dict['year_sem'] = student_details.year_sem
+        data_dict['shift'] = student_details.shift
+        data_dict['section'] = student_details.section
+        data_dict['door_num'] = student_details.door_num
+        data_dict['street_name'] = student_details.street_name
+        data_dict['city'] = student_details.city
+        data_dict['pincode'] = student_details.pincode
+        data_dict['district'] = student_details.district
+        data_dict['mandal'] = student_details.mandal
+        data_dict['phone_number'] = student_details.phone_number
+        data_dict['email'] = student_details.email
+        return HttpResponse(json.dumps(data_dict))
+    else:
+        return HttpResponse("Record NOT FOUND")
